6f6f59b37bb0d7e782e0ac697ff6e693cbc382ca
SmileyPirate1 <163312834+SmileyPirate1@users.noreply.github.com>
2025-09-19 09:20:38 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hej og velkommen til gæt et tal spillet");
+        System.out.println("Til at starte med skal du vælge en sværhedsgrad (let, mellem, svær)");
+
+        String difficulty = input.nextLine();
+
+    }
+}
