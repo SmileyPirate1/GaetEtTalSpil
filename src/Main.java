d705f50828d25c332c7df511db424aa79644148c
SmileyPirate1 <163312834+SmileyPirate1@users.noreply.github.com>
2025-09-19 10:58:19 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,75 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Hej og velkommen til gæt et tal spillet");
-        System.out.println("Til at starte med skal du vælge en sværhedsgrad (let, mellem, svær)");
 
-        String difficulty = input.nextLine();
         int chosenDifficulty = 0;
+//        boolean correct = true;
+        boolean play = true;
+
+        do {
+
+            System.out.println("Til at starte med skal du vælge en sværhedsgrad (let, mellem, svær)");
+            String difficulty = input.nextLine();
+
+//            do {
+//                if (difficulty.equals("let")) {
+//                    chosenDifficulty = 10;
+//                } else if (difficulty.equals("mellem")) {
+//                    chosenDifficulty = 50;
+//                } else if (difficulty.equals("svær")) {
+//                    chosenDifficulty = 100;
+//                } else {
+//                    System.out.println("vælg en sværhedsgrad");
+//                    correct = false;
+//                    difficulty = input.nextLine();
+//                }
+//            } while (correct == false );
+
+            int choosenDifficulty = chooseDifficulty(difficulty);
+
+            int number = 1 + (int)(choosenDifficulty * Math.random());
+
+
+
+            for (int attempts = 1; attempts <= 3; attempts++) {
+                System.out.println("gæt et tal: ");
+                int guess = input.nextInt();
+
+                if (guess == number) {
+                    System.out.println("Tillykke, du har gættet rigtigt på " + attempts + " forsøg");
+                    break;
+                } else if (guess < number) {
+                    System.out.println("Dit gæt var under tallet");
+                } else {
+                    System.out.println("Du gættede for højt");
+                }
+            }
+
+            System.out.println("du har brugt alle dine gæt, vil du spille igen?");
+            String again = input.nextLine();
+            again = input.nextLine();
+
+            switch (again) {
+                case "ja":
+                    play=true;
+                    break;
+                case "nej":
+                    play=false;
+                    break;
+            }
+
+
+        } while (play);
+
+
+
+    }
+
+    public static int chooseDifficulty(String difficulty) {
+        Scanner input = new Scanner(System.in);
+
         boolean correct = true;
+        int chosenDifficulty = 0;
 
         do {
             if (difficulty.equals("let")) {
@@ -27,5 +91,6 @@ public class Main {
             }
         } while (correct == false );
 
+        return (chosenDifficulty);
     }
 }
